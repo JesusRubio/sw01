fe7166b7e56124cc4fbc33c213ff01f8c5dfb4b7
Jesus Rubio <jrubio@lithographix.com>
2023-09-16 10:31:43 -0700

    Initial Commit

---
+++ Sources/main.swift
@@ -0,0 +1,72 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+print("Hello, world!");
+
+let mx = 10;
+var cur = 0;
+
+var x = 0.0, y=0.0, z = 0.0;
+
+let r = 1;
+
+print("hisis \(r)");
+
+let 🐶 = "Dog";
+let 🐮: String;
+
+🐮 = "cow";
+
+print(🐮);
+
+let minvalue = UInt32.min;
+
+print(minvalue);
+
+let pi = 3.14;
+
+print(pi);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
